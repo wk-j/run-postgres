b2b112616a4a7bf8dc36425f6210aa17c2314a4f
wk-j <somnuk.wk@gmail.com>
2018-09-22 01:19:31 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Run PostgreSQL (Docker)
+
+```bash
+dotnet tool install -g wk.RunPostgres
+wk-run-postgres
+```
+
+## Connection
+
+```bash
+Host=localhost; User Id=postgres; Password=1234; Database=postgres
+```
---
+++ src/RunPostgres/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open StartProcess
+open System.IO
+
+let createDir() =
+    let dir = ".working/db"
+    if Directory.Exists dir |> not then
+        Directory.CreateDirectory |> ignore
+    DirectoryInfo(dir).FullName
+
+[<EntryPoint>]
+let main argv =
+    let dir = createDir()
+
+    "docker stop postgres"
+    |> Processor.StartProcess
+
+    "docker rm postgres"
+    |> Processor.StartProcess
+
+    [ "docker run"
+      "-d"
+      "--name postgres"
+      "-v {db}:/var/lib/postgresql/data".Replace("{db}", dir)
+      "-e POSTGRES_PASSWORD=1234"
+      "-e POSTGRES_USER=postgres"
+      "-p 5432:5432"
+      "postgres" ]
+    |> String.concat " "
+    |> Processor.StartProcess
+
+    0
